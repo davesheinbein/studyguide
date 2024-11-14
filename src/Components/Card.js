1b6732afe9366dde07b21631cb48b79bee57447c
David Sheinbein <davidsheinbeindev@gmail.com>
2024-11-14 10:34:35 -0800

    update card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,18 +4,45 @@ import cheatsheetData from '../data/cheatsheet.json';
 
 const Card = () => {
 	const [cheatsheet, setCheatsheet] = useState([]);
+	const [expandedIndex, setExpandedIndex] = useState(null);
 
 	useEffect(() => {
 		setCheatsheet(cheatsheetData);
 	}, []);
 
+	const handleCardClick = (index) => {
+		setExpandedIndex(
+			expandedIndex === index ? null : index
+		);
+	};
+
+	const handleCloseClick = () => {
+		setExpandedIndex(null);
+	};
+
 	return (
 		<div className='card'>
 			<div className='card-grid'>
 				{cheatsheet.map((item, index) => (
-					<div key={index} className='card-item'>
+					<div
+						key={index}
+						className={`card-item ${
+							expandedIndex === index ? 'expanded' : ''
+						}`}
+						onClick={() => handleCardClick(index)}
+					>
+						{expandedIndex === index && (
+							<button
+								className='close-button'
+								onClick={handleCloseClick}
+							>
+								&times;
+							</button>
+						)}
 						<div className='card-item-topic'>
-							{item.topic}
+							<div className='card-item-topic-text'>
+								{item.topic}
+							</div>
 						</div>
 						<div className='card-item-code'>
 							<pre>{item.code}</pre>
